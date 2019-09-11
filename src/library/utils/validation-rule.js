2756b4ef34d95e9bc18ccf91cc00b0e5aef5ca20
ccsherry <wuchanyu07@163.com>
2019-09-11 10:39:25 +0800

    模板

---
+++ src/library/utils/validation-rule.js
@@ -0,0 +1,168 @@
+import {getStringByteLength, stringFormat} from './index';
+import * as regexps from './regexp';
+import _ from "lodash";
+
+export default {
+    ip(message = '请输入正确的IP地址！') {
+        return {
+            pattern: regexps.ip,
+            message,
+        };
+    },
+    port(message = '请输入正确的端口号！') {
+        return {
+            pattern: regexps.port,
+            message,
+        };
+    },
+    noSpace(message = '不能含有空格！') {
+        return {
+            validator: (rule, value, callback) => {
+                if (/\s/g.test(value)) return callback(message);
+                return callback();
+            },
+        };
+    },
+
+    mobile(message = '请输入正确的手机号！') { // 手机号
+        return {
+            pattern: regexps.mobile,
+            message,
+        };
+    },
+
+    landline(message = '请输入正确的座机号！') { // 座机
+        return {
+            pattern: regexps.landLine,
+            message,
+        };
+    },
+
+    qq(message = '请输入正确的qq号！') { // qq号
+        return {
+            pattern: regexps.qq,
+            message,
+        };
+    },
+
+    cardNumber(message = '请输入正确的身份证号！') { // 身份证号十五位十八位最后X的校验
+        return {
+            pattern: regexps.cardNumber,
+            message,
+        };
+    },
+
+    email(message = '请输入正确的邮箱！') {
+        return {
+            type: 'email',
+            message,
+        };
+    },
+
+    number(message = '请输入数字.') { // 纯数字，不包括 + -
+        return {
+            pattern: regexps.number,
+            message,
+        };
+    },
+    integer(message = '请输入整数！') { // 整数
+        return {
+            pattern: regexps.integer,
+            message,
+        };
+    },
+    positiveInteger(message = '请输入正整数！') { // 正整数 = 不按包含0
+        return {
+            pattern: regexps.positiveInteger,
+            message,
+        };
+    },
+    numberWithTwoDecimal(message = '请输入数字，保存两位小数.') {
+        return {
+            pattern: regexps.numberWithTwoDecimal,
+            message,
+        };
+    },
+    numberRange(min, max, message = '请输入{min}到{max}之间的值.') {
+        return {
+            validator(rule, value, callback) {
+                value = Number(value);
+
+                if (!value && value !== 0) return callback();
+
+                (value < min || value > max) ? callback(stringFormat(message, {min, max})) : callback();
+            },
+        };
+    },
+    numberMaxRange(max, message = '不能大于{max}') {
+        return {
+            validator(rule, value, callback) {
+                value = Number(value);
+
+                if (!value && value !== 0) return callback();
+
+                value > max ? callback(stringFormat(message, {max})) : callback();
+            },
+        };
+    },
+    numberMinRange(min, message = '不能小于{min}') {
+        return {
+            validator(rule, value, callback) {
+                value = Number(value);
+
+                if (!value && value !== 0) return callback();
+
+                value < min ? callback(stringFormat(message, {min})) : callback();
+            },
+        };
+    },
+
+    stringByteRangeLength(min, max, message = '请输入 {min}-{max} 个字符(汉字算2个字符).') {
+        return {
+            validator(rule, value, callback) {
+                if (!value) return callback();
+                let length = getStringByteLength(value);
+                (length < min || length > max) ? callback(stringFormat(message, {min, max})) : callback();
+            },
+        };
+    },
+    stringByteMinLength(min, message = '最少输入{min}个字符(汉字算2个字符).') {
+        return {
+            validator(rule, value, callback) {
+                if (!value) return callback();
+                let length = getStringByteLength(value);
+                length < min ? callback(stringFormat(message, {min})) : callback();
+            },
+        };
+    },
+    stringByteMaxLength(max, message = '最多输入{max}个字符(汉字算2个字符).') {
+        return {
+            validator(rule, value, callback) {
+                if (!value) return callback();
+                let length = getStringByteLength(value);
+                length > max ? callback(stringFormat(message, {max})) : callback();
+            },
+        };
+    },
+
+    arrayMaxLength(max, message = '最多{max}个值') {
+        return {
+            validator(rule, value, callback) {
+                if (!value || !Array.isArray(value)) return callback();
+                let length = value.length;
+                length > max ? callback(stringFormat(message, {max})) : callback();
+            },
+        };
+    },
+
+    // 截流校验写法
+    userNameExist() {
+        if (!this._userNameExist) this._userNameExist = _.debounce((rule, value, callback) => {
+            console.log('发请求');
+        }, 500);
+
+        return {
+            validator: this._userNameExist
+        }
+    },
+};
---
+++ src/pages/users/index.jsx
@@ -0,0 +1,168 @@
+import React, {Component} from 'react';
+import {Button, Table, Form} from 'antd';
+import PageContent from '@/layouts/page-content';
+import {
+    QueryBar,
+    Pagination,
+    Operator,
+    ToolBar,
+    FormRow,
+    FormElement,
+} from "@/library/antd";
+import config from '@/commons/config-hoc';
+import UserEditModal from './UserEditModal';
+
+@config({
+    path: '/users',
+    ajax: true,
+})
+@Form.create()
+export default class UserCenter extends Component {
+    state = {
+        dataSource: [],     // 表格数据
+        total: 0,           // 分页中条数
+        pageSize: 10,       // 分页每页显示条数
+        pageNum: 1,         // 分页当前页
+        collapsed: true,    // 是否收起
+        visible: false,     // 添加、修改弹框
+        id: null,           // 需要修改的数据id
+    };
+
+    columns = [
+        {title: '用户名', dataIndex: 'name', key: 'name'},
+        {title: '年龄', dataIndex: 'age', key: 'age'},
+        {title: '工作', dataIndex: 'job', key: 'job'},
+        {title: '职位', dataIndex: 'position', key: 'position'},
+        {
+            title: '操作', dataIndex: 'operator', key: 'operator',
+            render: (value, record) => {
+                const {id, name} = record;
+                const items = [
+                    {
+                        label: '编辑',
+                        onClick: () => this.setState({visible: true, id}),
+                    },
+                    {
+                        label: '删除',
+                        color: 'red',
+                        confirm: {
+                            title: `您确定删除"${name}"?`,
+                            onConfirm: () => this.handleSearch(),
+                        },
+                    }
+                ];
+
+                return <Operator items={items}/>
+            },
+        }
+    ];
+
+    componentDidMount() {
+        this.handleSearch();
+    }
+
+    handleSearch = () => {
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (err) return;
+            const {pageNum, pageSize} = this.state;
+
+            const params = {
+                ...values,
+                pageNum,
+                pageSize,
+            };
+
+            this.props.ajax.get('/xxx', params)
+                .then(res => {
+                    const dataSource = res?.list || [];
+                    const total = res?.total || 0;
+
+                    this.setState({dataSource, total});
+                });
+        });
+
+    };
+
+    FormElement = (props) => <FormElement form={this.props.form} labelWidth={62} width={300} style={{paddingLeft: 16}} {...props}/>;
+
+    render() {
+        const {
+            total,
+            pageNum,
+            pageSize,
+            collapsed,
+            dataSource,
+            visible,
+            id,
+        } = this.state;
+
+        const FormElement = this.FormElement;
+        return (
+            <PageContent>
+                <QueryBar
+                    showCollapsed
+                    collapsed={collapsed}
+                    onCollapsedChange={collapsed => this.setState({collapsed})}
+                >
+                    <FormRow>
+                        <FormElement
+                            label="名称"
+                            field="name"
+                        />
+                        <FormElement
+                            type="select"
+                            label="职位"
+                            field="job"
+                            options={[
+                                {value: 1, label: 1},
+                                {value: 2, label: 2},
+                            ]}
+                        />
+                    </FormRow>
+                    <FormRow>
+                        <FormElement
+                            type="date"
+                            label="入职时间"
+                            field="time"
+                        />
+                        <FormElement
+                            label="年龄"
+                            field="age"
+                        />
+                        <FormElement layout width="auto">
+                            <Button type="primary" onClick={this.handleSearch}>提交</Button>
+                            <Button onClick={() => this.props.form.resetFields()}>重置</Button>
+                        </FormElement>
+                    </FormRow>
+                </QueryBar>
+
+                <ToolBar
+                    items={[
+                        {type: 'primary', text: '添加用户', icon: 'user-add', onClick: () => this.setState({visible: true, id: null})}
+                    ]}
+                />
+
+                <Table
+                    columns={this.columns}
+                    dataSource={dataSource}
+                    rowKey="id"
+                    pagination={false}
+                />
+
+                <Pagination
+                    total={total}
+                    pageNum={pageNum}
+                    pageSize={pageSize}
+                    onPageNumChange={pageNum => this.setState({pageNum}, this.handleSearch)}
+                    onPageSizeChange={pageSize => this.setState({pageSize, pageNum: 1}, this.handleSearch)}
+                />
+                <UserEditModal
+                    visible={visible}
+                    id={id}
+                    onOk={() => this.setState({visible: false}, this.handleSearch)}
+                    onCancel={() => this.setState({visible: false})}
+                />
+            </PageContent>
+        );
+    }
+}
